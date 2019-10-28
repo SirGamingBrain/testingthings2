8db1b1ce5df27c3905965cb40c3137ae92548e7d
Setsuisho <mari4hawkins@gmail.com>
2019-10-28 15:17:03 -0400

    Enemy Particles
    Modded the particles for the various states of the enemies.
    Edited text prompts a little more as well.

--- Assets/Scripts/Enemies/ParticleModifier.cs
+++ Assets/Scripts/Enemies/ParticleModifier.cs
@@ -6,15 +6,31 @@ public class ParticleModifier : MonoBehaviour
 {
     public EnemyBase enemy;
     public ParticleSystem spores;
+    public ParticleSystem.MainModule sporeMain;
     public ParticleSystem.EmissionModule sporeEmission;
-    
+    public ParticleSystem.ColorOverLifetimeModule sporeColor;
+
+    private Gradient sporeAlertGradient;
+    private Gradient sporeWaitingGradient;
+
     // Start is called before the first frame update
     void Start()
     {
         enemy = this.gameObject.GetComponent<EnemyBase>();
-        //spores = GetComponent<ParticleSystem>();
+
+        sporeMain = spores.main;
+
         sporeEmission = spores.emission;
-        
+
+        float alpha = 1.0f;
+        sporeColor = spores.colorOverLifetime;
+        sporeColor.color = Color.white;
+        sporeAlertGradient = new Gradient();
+        sporeAlertGradient.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.red, 1.0f), new GradientColorKey(Color.red, 0.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 1.0f), new GradientAlphaKey(alpha, 0.0f) });
+        sporeWaitingGradient = new Gradient();
+        sporeWaitingGradient.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.yellow, 1.0f), new GradientColorKey(Color.yellow, 0.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 1.0f), new GradientAlphaKey(alpha, 0.0f) });
     }
 
     // Update is called once per frame
@@ -32,12 +48,16 @@ public class ParticleModifier : MonoBehaviour
 
         if (enemy.alertStatus == true)
         {
+            
             sporeEmission.rateOverTime = new ParticleSystem.MinMaxCurve(5f, 10f);
+            sporeColor.color = sporeAlertGradient;
+            sporeMain.maxParticles = 8;
         }
 
         else if (enemy.distracted == true)
         {
             sporeEmission.rateOverTime = new ParticleSystem.MinMaxCurve(1f, 3f);
+            sporeColor.color = sporeWaitingGradient;
         }
 
         else if (enemy.freeze == true)
@@ -53,6 +73,8 @@ public class ParticleModifier : MonoBehaviour
         else
         {
             sporeEmission.rateOverTime = 5f;
+            sporeColor.color = Color.white;
+            sporeMain.maxParticles = 5;
         }
     }
 }
---
+++ Assets/Scripts/Level Scripts/TextPrompts.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TextPrompts : MonoBehaviour
+{
+    private Scene scene;
+
+    public Text playerWords;
+    public CanvasGroup playerText;
+    public float textTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerText.alpha = 1f;
+        textTimer = 0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        textTimer += Time.deltaTime;
+        if (scene.name == "1st Level")
+        {
+            playerText.alpha = 1f;
+            playerWords.text = "The taser broke during the run; I'll need special supplies to fix it.";
+            
+        }
+
+        if(textTimer > 5f)
+        {
+            playerText.alpha = 0f;
+            playerWords.text = "";
+        }
+
+        
+    }
+}
