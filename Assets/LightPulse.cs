4f132c9fa6140ab0955f20241cf2628f5dfaa2fc
Setsuisho <mari4hawkins@gmail.com>
2019-11-03 14:08:35 -0500

    Lights
    Added light pulse plus lanterns for level 2

---
+++ Assets/LightPulse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPulse : MonoBehaviour
+{
+    public Light consoleLight;
+    public float bounceTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        consoleLight = this.gameObject.GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flux();
+    }
+
+    public void Flux()
+    {
+        float pulse = Mathf.PingPong(Time.time, bounceTime);
+        consoleLight.intensity = pulse;
+    }
+}
